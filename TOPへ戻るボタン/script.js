f0ea4aa54704e8d8059184004512d6c43c9dbabd
lenrendraw1114 <lenrendraw1114@gmail.com>
2025-01-26 17:56:19 +0900

    UX改善

--- TOPへ戻るボタン/script.js
+++ TOPへ戻るボタン/script.js
@@ -1,24 +1,20 @@
-// IE、Safari対応
-// smoothscroll.js読み込み
-// https://github.com/iamdustan/smoothscroll
-
 // セレクタ名（.pagetop）に一致する要素を取得
-const pagetop_btn = document.querySelector(".pagetop");
+const topBtn = document.querySelector(".pagetop");
 
-// .pagetopをクリックしたら
-pagetop_btn.addEventListener("click", scroll_top);
+// まず、スクロールされたらボタンを表示
+window.addEventListener("scroll", scroll_event);
+function scroll_event() {
+  if (window.scrollY > 100) {
+    topBtn.style.opacity = "1";
+  } else if (window.scrollY < 100) {
+    topBtn.style.opacity = "0";
+  }
+}
+
+// そして、.pagetopをクリックしたら
+topBtn.addEventListener("click", scroll_top);
 
 // ページ上部へスムーズに移動
 function scroll_top() {
-  window.scroll({ top: 0, behavior: "smooth" });
-}
-
-// スクロールされたら表示
-window.addEventListener("scroll", scroll_event);
-function scroll_event() {
-  if (window.pageYOffset > 100) {
-    pagetop_btn.style.opacity = "1";
-  } else if (window.pageYOffset < 100) {
-    pagetop_btn.style.opacity = "0";
-  }
+  window.scrollTo(0, 0);
 }
